0741ae0a7e047438c159a3a48eef5a80de2af0d1
lukasz <plszymanlu@gmail.com>
2020-11-02 19:18:59 +0100

    NewProps

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,13 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}> My first React app </h1>
         <h2 className={styles.subtitle}> Hello world! </h2>
-        <List />
+        <List
+          title={['Things to do ', <sup>soon!</sup>]}
+          img={'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'}
+          colTitle1={'Animals'}
+          colTitle2={'Planets'}
+          colTitle3={'Minerals'}
+        ></List>
       </main>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
-  render() {
-    return (
-      <section className={styles.component}>
-        <Hero />
-      </section>
-    );
-  }
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node,
+        img: PropTypes.string,
+        colTitle1: PropTypes.string,
+        colTitle2: PropTypes.string,
+        colTitle3: PropTypes.string,
+    };
+
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    };
+
+    render() {
+        return (
+            <section className={styles.component}>
+                <Hero titleText={this.props.title} imgSrc={this.props.img} />
+                <div className={styles.description}>{this.props.children}</div>
+                <div className={styles.columns}>
+                    <Column columnTitle={this.props.colTitle1} />
+                    <Column columnTitle={this.props.colTitle2} />
+                    <Column columnTitle={this.props.colTitle3} />
+                </div>
+            </section>
+        );
+    }
 }
 
 export default List;
